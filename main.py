64c7d2404a2a2efae9b63099dc6f3186ee73c02a
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-10-30 16:57:51 -0500

    solucion ordenamiento parte 3

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-numeroONe=int(input("Enter a number"))
-numeroTwo=int(input("Enter a number"))
+# Solicitar la entrada de cuatro números
+numero1 = int(input("Ingrese número: "))
+numero2 = int(input("Ingrese número: "))
+numero3 = int(input("Ingrese número: "))
+numero4 = int(input("Ingrese número: "))
 
-if numeroONe<numeroTwo:
-    print(f"""
-       
-        {numeroONe, numeroTwo}
-""")
-elif numeroTwo<numeroONe:    
-    print(f"""       
-        {numeroTwo, numeroONe}
-""")
-    
+# Crear una lista con los números
+numeros = [numero1, numero2, numero3, numero4]
+
+# Ordenar la lista
+numeros.sort()
+
+# Mostrar los números en orden
+print(" ".join(map(str, numeros)))
