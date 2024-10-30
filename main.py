f04ba59128773c3ec6f456e93e632cc9669d9a22
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-10-30 17:17:31 -0500

    Solucion ejercicio calculadora

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-# Solicitar la entrada de un carácter
-caracter = input("Ingrese caracter: ")
+# Solicitar el primer operando
+operando1 = float(input("Operando: "))
 
-# Verificar si es un número
-if caracter.isdigit():
-    print("Es número.")
-# Verificar si es una letra
-elif caracter.isalpha():
-    if caracter.isupper():
-        print("Es letra mayúscula.")
+# Solicitar el operador
+operador = input("Operador: ")
+
+# Solicitar el segundo operando
+operando2 = float(input("Operando: "))
+
+# Realizar la operación según el operador
+if operador == '+':
+    resultado = operando1 + operando2
+    print(f"{operando1} + {operando2} = {resultado}")
+elif operador == '-':
+    resultado = operando1 - operando2
+    print(f"{operando1} - {operando2} = {resultado}")
+elif operador == '*':
+    resultado = operando1 * operando2
+    print(f"{operando1} * {operando2} = {resultado}")
+elif operador == '/':
+    if operando2 != 0:  # Comprobar si el divisor no es cero
+        resultado = operando1 / operando2
+        print(f"{operando1} / {operando2} = {resultado}")
     else:
-        print("Es letra minúscula.")
-# Si no es ni letra ni número
+        print("Error: División por cero no está permitida.")
+elif operador == '**':
+    resultado = operando1 ** operando2
+    print(f"{operando1} ** {operando2} = {resultado}")
 else:
-    print("No es letra ni número.")
+    print("Error: Operador no válido.")
