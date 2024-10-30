1d2d1ce382c364d4c831096d9af04b07d0e83e05
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-10-30 16:57:15 -0500

    solucion ordenamiento parte uno

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 numeroONe=int(input("Enter a number"))
 numeroTwo=int(input("Enter a number"))
-numeroThre=int(input("Enter a number"))
 
-
-if numeroONe<numeroTwo<numeroThre: #uno, dos, tres
+if numeroONe<numeroTwo:
     print(f"""
        
-        {numeroONe, numeroTwo, numeroThre}
+        {numeroONe, numeroTwo}
 """)
-elif numeroTwo<numeroONe<numeroThre: #dos, uno, tres     
+elif numeroTwo<numeroONe:    
     print(f"""       
-        {numeroTwo, numeroONe, numeroThre}
+        {numeroTwo, numeroONe}
 """)
-elif numeroONe<numeroThre<numeroTwo: #uno, tres, dos
-    print(f"""       
-        {numeroONe,numeroThre, numeroTwo}
-""")
-elif numeroThre<numeroONe<numeroTwo: #tres, uno, dos
-    print(f"""       
-        {numeroThre,numeroONe, numeroTwo}
-""")
-elif numeroTwo<numeroThre<numeroONe: #two, tres, one
-    print(f"""       
-        {numeroTwo,numeroThre, numeroONe}
-""")
-elif numeroThre<numeroTwo<numeroONe: #tres, dos, uno
-    print(f"""       
-        {numeroThre,numeroTwo, numeroONe}
-""")    
+    
