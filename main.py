4f39300daaf8ec2e511623b985ed4b3c11cb15d4
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-10-30 15:42:18 -0500

    solucion años bisisestos

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-number=int(input("Enter one integer number: "))
+# Solicita al usuario que ingrese un año
+año = int(input("Pon tu año y yo te diré si es bisiesto: "))
 
-if (number%4)==0:
+# Verifica si el año es divisible por 100
+divisible100 = (año % 100) == 0
 
-     print("your number is par")
-
+# Comienza a verificar si el año es bisiesto
+if (año % 4) == 0:
+    # Si el año es divisible por 100, se debe verificar si es también divisible por 400
+    if divisible100:
+        if (año % 400) == 0:
+            print(f"{año} es bisiesto")
+        else:
+            print(f"{año} no es bisiesto")
+    else:
+        print(f"{año} es bisiesto")
 else:
-
-     print("your number is impar")
+    print(f"{año} no es bisiesto")
